408366941daced86f4f203bfaf35c35367c5ceea
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-10-14 14:00:16 -0400

    Updated from Brython Server: 10/14/2016 2:00:15 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,3 +21,8 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+
+for j in range (1,9):
+    for i in range (1, 11):
+        print(i, end = " ")
+    print() 
