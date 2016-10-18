f97de1590c10f8ee26bd5c464c4b60740ac5b2fa
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-10-18 13:19:41 -0400

    Updated from Brython Server: 10/18/2016 1:19:41 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author:jonh warhold
+Credit: 
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
